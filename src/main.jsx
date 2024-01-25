3eac2cf5d965b679f71a0becef1bf8c1f5e28d09
Saurav kumar verma <sauravkumarverma22@gmail.com>
2024-01-25 23:10:34 +0530

    Making the component Flexible using Props API

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import App from "./App.jsx";
+// import "./index.css";
+import StarRating from "./StarRating.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <StarRating defaultRating={4} />
+    <StarRating defaultRating={9} maxRating={10} size={24} color="purple" />
+    <StarRating defaultRating={13} maxRating={15} classNme="test" />
+    <StarRating
+      defaultRating={3}
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+  </React.StrictMode>
+);
