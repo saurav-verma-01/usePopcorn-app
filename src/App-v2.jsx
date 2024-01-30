db630a1033c147c4780c25433be6fcd3df9bb2ae
Saurav kumar verma <sauravkumarverma22@gmail.com>
2024-01-30 12:25:37 +0530

    Learned useRef Hook and useCases

--- src/App-v2.jsx
+++ src/App-v2.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import StarRating from "./StarRating";
 
 const average = (arr) =>
@@ -161,6 +161,23 @@ function Logo() {
 }
 
 function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+
+  useEffect(() => {
+    if (document.activeElement === inputEl.current) return;
+
+    const callback = (e) => {
+      if (e.code === "Enter") {
+        inputEl.current.focus();
+        setQuery("");
+      }
+    };
+
+    document.addEventListener("keydown", callback);
+    return () => document.removeEventListener("keydown", callback);
+
+    // inputEl.current.focus();
+  }, [setQuery]);
   return (
     <input
       className="search"
@@ -168,6 +185,7 @@ function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
